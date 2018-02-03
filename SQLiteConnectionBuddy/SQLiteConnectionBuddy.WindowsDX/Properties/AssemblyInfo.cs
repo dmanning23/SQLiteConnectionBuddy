8a18e5ed5df51247ebfcbd179e4acd92360e57c1
Dan Manning <dmanning23@hotmail.com>
2018-02-02 23:16:50 -0600

    moved to windx

--- SQLiteConnectionBuddy/SQLiteConnectionBuddy.WindowsDX/Properties/AssemblyInfo.cs
+++ SQLiteConnectionBuddy/SQLiteConnectionBuddy.WindowsDX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SQLiteConnectionBuddy.DesktopGL")]
+[assembly: AssemblyTitle("SQLiteConnectionBuddy.WindowsDX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SQLiteConnectionBuddy.DesktopGL")]
+[assembly: AssemblyProduct("SQLiteConnectionBuddy.WindowsDX")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
