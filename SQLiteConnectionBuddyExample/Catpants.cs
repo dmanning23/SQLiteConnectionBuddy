427c8730f1f5053db2233acbd1c2ac44177349c4
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2026-01-17 20:56:59 -0600

    Added sample project

---
+++ SQLiteConnectionBuddyExample/Catpants.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace SQLiteConnectionBuddyExample
+{
+    [Table("Cats")]
+    public class Catpants
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(32)]
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+    }
+}
---
+++ SQLiteConnectionBuddyExample/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new SQLiteConnectionBuddyExample.Game1();
+game.Run();
